b1d79eb8262f5d22d5963ff9a6cc23240ae2acf6
unknown <widya160499@gmail.com>
2022-02-01 12:04:43 +0800

    create migration kegiatan

---
+++ database/migrations/2022_02_01_040229_create_kegiatans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateKegiatansTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('kegiatans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_progra',100);
+            $table->integer('tahun_anggaran');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('kegiatans');
+    }
+}
