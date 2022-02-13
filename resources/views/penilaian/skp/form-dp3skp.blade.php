68027ad25e44053b4b8197f5d7395ee8aa0accc5
unknown <widya160499@gmail.com>
2022-02-13 18:15:29 +0800

    update dp-3

--- resources/views/penilaian/skp/form-dp3skp.blade.php
+++ resources/views/penilaian/skp/form-dp3skp.blade.php
@@ -27,40 +27,40 @@
             <td style="width: 100px">Nama</td>
             <td style="width: 300px"></td>
             <td>1</td>
-            <td style="width: 100px">Nama</td>
-            <td colspan="5"></td>
+            <td colspan="2">Nama</td>
+            <td colspan="4"></td>
         </tr>
         <tr>
             <td>2</td>
             <td style="width: 100px">NIP</td>
             <td></td>
             <td>2</td>
-            <td style="width: 100px">No Kontrak</td>
-            <td colspan="5"></td>
+            <td colspan="2">No Kontrak</td>
+            <td colspan="4"></td>
         </tr>
         <tr>
             <td>3</td>
             <td style="width: 100px">Pangkat/Gol.Ruang</td>
             <td></td>
             <td>3</td>
-            <td style="width: 100px">Pangkat/Gol.Ruang</td>
-            <td colspan="5"></td>
+            <td colspan="2">Pangkat/Gol.Ruang</td>
+            <td colspan="4"></td>
         </tr>
         <tr>
             <td>4</td>
             <td style="width: 100px">Jabatan</td>
             <td></td>
             <td>4</td>
-            <td style="width: 100px">Jabatan</td>
-            <td colspan="5"></td>
+            <td colspan="2">Jabatan</td>
+            <td colspan="4"></td>
         </tr>
         <tr>
             <td>5</td>
             <td style="width: 100px">Unit Kerja</td>
             <td></td>
             <td>5</td>
-            <td style="width: 100px">Unit Kerja</td>
-            <td colspan="5"></td>
+            <td colspan="2">Unit Kerja</td>
+            <td colspan="4"></td>
         </tr>
         <tr>
             <th rowspan="2" style="width: 10px">NO</th>
@@ -78,14 +78,37 @@
             <td>1</td>
             <td colspan="2"> ssss</td>
             <td>-</td>
-            <td style="width: 10px">0</td>
+            <td>0</td>
             <td>Laporan</td>
             <td>0</td>
             <td>0</td>
             <td>Bulan</td>
             <td></td>
+        </tr>
+        <tr></tr>
+    </table>
 
-
+    <table style="border:none;">
+        <tr style="border:none;">
+            <td style="width: 100px"></td>
+            <td style="width: 200px">
+                <br>
+                <br>
+                Pejabat Penilai
+                <br><br><br><br><br>
+                Nama <br>
+                NIP
+            </td>
+            <td style="width: 500px"></td>
+            <td style="width: 300px">
+                Denpasar, {{ \Carbon\Carbon::now()->isoFormat('LL') }} <br>
+                Pegawai Kontrak Yang Dinilai
+                <br><br><br><br><br>
+                {{ Auth::user()->name }}
+                Nama <br>
+                NIP
+            </td>
+            <td style="width: 100px"></td>
         </tr>
     </table>
 </div>
