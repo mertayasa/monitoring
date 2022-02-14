adeb48335ffa1de5ff34c558c7106cada8ee93a7
unknown <widya160499@gmail.com>
2022-02-14 16:21:20 +0800

    update ppk

--- resources/views/penilaian/skp/form-ppk.blade.php
+++ resources/views/penilaian/skp/form-ppk.blade.php
@@ -2,36 +2,30 @@
     <table class="table table-bordered skp" style="color: black; font-size:10px; padding:0; ">
         <tr>
             <td colspan="3" style="text-align:left">8. REKOMENDASI</td>
-            <td></td>
+            <td rowspan="24"></td>
         </tr>
         <tr>
             <td colspan="3" rowspan="4"></td>
-            <td></td>
-            <td colspan="4" style="text-align: center"> <img src="{{ asset('admin/img/pancasila-color.png') }}"
+            <td colspan="3" style="text-align: center"> <img src="{{ asset('admin/img/pancasila-color.png') }}"
                     width="100px">
             </td>
         </tr>
         <tr>
-            <td></td>
-            <td colspan="4" style="text-align: center; line-height:15px ">PENILAIAN PRESTASI KERJA <br>
+            <td colspan="3" style="text-align: center; line-height:15px ">PENILAIAN PRESTASI KERJA <br>
                 PEGAWAI KONTRAK</td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
-            <td></td>
-            <td colspan="2">JANGKA WAKTU PENILAIAN</td>
+            <td colspan="2"> </td>
+            <td>JANGKA WAKTU PENILAIAN</td>
         </tr>
         <tr>
-            <td></td>
             <td colspan="2">PEMERINTAH KAB. KLUNGKUNG </td>
-            <td colspan="2">Tanggal : ... s/d ..</td>
+            <td>Tanggal : ... s/d ..</td>
         </tr>
         <tr>
             <td></td>
             <td></td>
             <td>9. DIBUAT TANGGAL , .. </td>
-            <td></td>
             <td>1.</td>
             <td colspan="2"> YANG DINILAI</td>
         </tr>
@@ -39,9 +33,8 @@
             <td></td>
             <td></td>
             <td>PEJABAT PENILAI</td>
-            <td></td>
-            <td></td>
-            <td>a. Nama</td>
+            <td rowspan="5"></td>
+            <td> a. Nama</td>
             <td>Ni Kade ...</td>
         </tr>
         <tr>
@@ -51,124 +44,135 @@
             <td></td>
             <td rowspan="17">
             </td>
-            <td></td>
-            <td></td>
-            <td>a. Nama</td>
-            <td>Ni Kade ...</td>
-        </tr>
-        <tr>
-            <td></td>
-            <td></td>
-            <td></td>
             <td>b. NIP</td>
             <td>...</td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
             <td></td>
             <td>c. Pangkat, Golongan Ruang, TMT</td>
             <td> ...</td>
         </tr>
         <tr>
             <td></td>
-            <td></td>
-            <td></td>
             <td>d. Jabatan/Pangkat</td>
             <td> ...</td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
             <td></td>
             <td>e. Unit Organisasi</td>
             <td> ...</td>
         </tr>
 
         <tr>
-            <td></td>
             <td></td>
             <td>2.</td>
             <td colspan="2"> PEJABAT PENILAI</td>
         </tr>
         <tr>
             <td></td>
-            <td></td>
-            <td></td>
+            <td rowspan="5"></td>
             <td>a. Nama</td>
             <td>Ni Kade ...</td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
             <td></td>
             <td>b. NIP</td>
             <td>...</td>
         </tr>
         <tr>
             <td></td>
-            <td></td>
-            <td></td>
             <td>c. Pangkat, Golongan Ruang, TMT</td>
             <td> ...</td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
             <td></td>
             <td>d. Jabatan/Pangkat</td>
             <td> ...</td>
         </tr>
         <tr>
             <td></td>
-            <td></td>
-            <td></td>
             <td>e. Unit Organisasi</td>
             <td> ...</td>
         </tr>
 
         <tr>
-            <td></td>
             <td></td>
             <td>3.</td>
             <td colspan="2"> ATASAN PEJABAT PENILAI</td>
         </tr>
         <tr>
             <td></td>
-            <td></td>
-            <td></td>
+            <td rowspan="5"></td>
             <td>a. Nama</td>
             <td>Ni Kade ...</td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
             <td></td>
             <td>b. NIP</td>
             <td>...</td>
         </tr>
         <tr>
             <td></td>
-            <td></td>
-            <td></td>
             <td>c. Pangkat, Golongan Ruang, TMT</td>
             <td> ...</td>
         </tr>
         <tr>
-            <td></td>
-            <td></td>
             <td></td>
             <td>d. Jabatan/Pangkat</td>
             <td> ...</td>
         </tr>
         <tr>
             <td></td>
-            <td></td>
-            <td></td>
             <td>e. Unit Organisasi</td>
             <td> ...</td>
         </tr>
+    </table>
 
+    <br>
+
+    <table class="table table-borderless ">
+        <tr>
+            <td colspan="3" style="text-align: center; color:black">RAHASIA</td>
+            <td></td>
+            <td colspan="3" style="text-align: center; color:black">RAHASIA</td>
+        </tr>
+    </table>
+
+    <table class="table table-bordered">
+        <tr>
+            <td>4.</td>
+            <td colspan="4">UNSUR YANG DINILAI</td>
+            <td>Jumlah</td>
+            <td></td>
+            <td>6.</td>
+            <td>TANGGAPAN PEJABAT PENILAI</td>
+            <td></td>
+            <td colspan="2"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td colspan="2">a. Sasaran Kerja Pegawai (SKP)</td>
+            <td>86,78</td>
+            <td>x 60%</td>
+            <td>51,00</td>
+            <td></td>
+            <td></td>
+            <td>ATAS KEBERATAN</td>
+            <td></td>
+            <td colspan="2"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>b. Perilaku Kerja</td>
+            <td>1. Orientasi Pelayanan</td>
+            <td>70</td>
+            <td>(Baik)</td>
+            <td rowspan="9"></td>
+            <td></td>
+            <td></td>
+            <td></td>
+            <td colspan="2"></td>
+        </tr>
     </table>
 
 </div>
